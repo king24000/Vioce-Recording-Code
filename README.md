51997297927c45b642ce5d79ef47c1518b83469a
Usama <104695332+king24000@users.noreply.github.com>
2023-10-27 19:13:24 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vioce-Recording-Code
+ 
---
+++ main.py
@@ -0,0 +1,14 @@
+import sounddevice
+from scipy.io.wavfile import write   
+from tkinter import * 
+from tkinter.messagebox import showinfo
+from tkinter.filedialog import askdirectory
+
+def main_window():
+    win = Tk()
+    
+    
+    
+    win.mainloop()
+    
+main_window()
